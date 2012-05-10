bfad0396d7a2ea5c90e61bd32ecf439f5f06ba63
Miles Sandlar <miles.sandlar@gmail.com>
2012-05-10 13:26:37 -0400

    Bare minimum for XCB connection and loop

--- mies-wm.c
+++ mies-wm.c
@@ -4,11 +4,9 @@
 #define NIL (0)
 
 main() {
-	//Connection, Screen, and Event
 	xcb_connection_t *xcbConnection;
 	xcb_generic_event_t *event;
 
-	//Connect and Setup Screen and Grab Btn
 	xcbConnection = xcb_connect(NIL, NIL);
 
 	for (;;) {
