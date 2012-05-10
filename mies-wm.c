0d8986e7579ccd2791b9a9263800fa99326a8eea
Miles Sandlar <miles.sandlar@gmail.com>
2012-05-10 00:41:54 -0400

    Basic handling for event.type, switch for mousebtn Considering ways to structure.

--- mies-wm.c
+++ mies-wm.c
@@ -5,6 +5,14 @@
 
 #define NIL (0)
 
+/*
+void mouseClick(XEvent * e, XButtonEvent * m, Display * d) {
+	XEvent event = * e;
+	XButtonEvent mouse = * m;
+	Display display = * d;
+}
+*/
+
 main() {
 	//Create and Assert Display
 	Display *dpy = XOpenDisplay(NIL); 
@@ -14,7 +22,6 @@ main() {
 	XSizeHints *hints;
 	XSetNormalHints(dpy, DefaultRootWindow(dpy), hints);
 
-
 	//Mouse Event
 	XButtonEvent mouse;
 	XEvent event;
@@ -22,11 +29,38 @@ main() {
 
 	// Loop X Events
 	mouse.subwindow = None;
+
+	/* main event loop */
 	for (;;) {
-		XNextEvent(dpy, &event);
+
+		XNextEvent(dpy, &event); 
+		
+		if (event.type == MotionNotify) {
+			//mouseClick( & event, & mouse, & dpy);
+
+			switch(mouse.button) {
+				case 1:
+					XMoveResizeWindow(
+							dpy, mouse.subwindow, 
+							event.xbutton.x_root - mouse.x_root,
+							event.xbutton.y_root - mouse.y_root,
+							500,500
+					);
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+			}
+
+
+		} else if (event.type == ButtonPress) {
+		} else if (event.type == KeyPress) {
+		}
 	}
 
 	//Cleanup	
 	XMapWindow(dpy, DefaultRootWindow(dpy));
 	XFlush(dpy);
 }
+
