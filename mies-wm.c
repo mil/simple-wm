650c75059846bcd9c058aa4c44e1fbee60186779
Miles Sandlar <miles.sandlar@gmail.com>
2012-05-10 12:34:24 -0400

    Waiting for events in XCB

--- mies-wm.c
+++ mies-wm.c
@@ -1,51 +1,25 @@
 // mies-wm
-#include <X11/Xlib.h>
-#include <X11/Xutil.h> // For Resizing
+#include <xcb/xcb.h>
 #include <assert.h> 
 
 #define NIL (0)
 
 main() {
-	//Create and Assert Display
-	Display *dpy = XOpenDisplay(NIL); 
-	assert(dpy); 
+	int screen;
+	xcb_connection_t *xcbConnection;
+	xcb_generic_event_t *event;
 
-	//Hints
-	XSizeHints *hints;
-	XSetWMNormalHints(dpy, DefaultRootWindow(dpy), hints);
+	xcbConnection = xcb_connect(
+		NIL,
+		&screen
+	);   
 
-	//Mouse Event
-	XButtonEvent mouse;
-	XEvent event;
-
-	//Grab Key
-    XGrabKey(dpy, XKeysymToKeycode(
-		dpy, XStringToKeysym("XK_ESCAPE")), Mod1Mask,
-		DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync
-	);
-
-
-	// Loop X Events
-	mouse.subwindow = None;
 	for (;;) {
-		XNextEvent(dpy, &event); 
-		
-		if (event.type == MotionNotify) {
-		} else if (event.type == ButtonPress) {
-		} else if (event.type == KeyPress) {
-		}
-	
-		/*
-		if (mouse.subwindow) {
-			XCirculateSubwindowsDown(dpy, mouse.subwindow);
-		}
-		*/
-		XSync(dpy, True);
-		mouse.subwindow = None;
+		event = xcb_wait_for_event(xcbConnection);
 	}
 
-	//Cleanup	
-	XMapWindow(dpy, DefaultRootWindow(dpy));
-	XFlush(dpy);
-}
+	xcb_flush(xcbConnection);
+	xcb_disconnect(xcbConnection);
 
+	return 0;
+}
