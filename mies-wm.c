f5121689c4694540c1ece60f3157d65b5d6a2835
Miles Sandlar <miles.sandlar@gmail.com>
2012-05-12 01:01:03 -0400

    Experimenting with Xlib's Event Mask

--- mies-wm.c
+++ mies-wm.c
@@ -1,15 +1,81 @@
 // mies-wm
-#include <xcb/xcb.h>
+#include <assert.h>
+#include <string.h>
+#include <X11/Xlib.h>
 
 #define NIL (0)
 
-main() {
-	xcb_connection_t *xcbConnection;
-	xcb_generic_event_t *event;
+//Screen and Display
+int 			screen;
+int				activeScreen;
+Display			*display;
 
-	xcbConnection = xcb_connect(NIL, NIL);
+//Windows
+Window 			window; 
+
+//Event
+XEvent			event;
+
+
+//Display message 
+void displayMessage() {
+	XDrawString(
+			display, 
+			window, 
+			DefaultGC(display, activeScreen), 
+			50, 50, 
+			"mmm wm", strlen("mmm wm")
+	);	
+
+}
+
+//Creates a window
+int createWindow() {
+	window = XCreateSimpleWindow(
+			 display, 
+			 RootWindow(display, activeScreen), 
+			 10, 10, 
+			 100, 200, 1,
+			 BlackPixel(display, activeScreen), 
+			 WhitePixel(display, activeScreen)
+	);	
+    XMapWindow(display, window);
+
+	return window;
+}
+
+//Centers the pointer in the middle of the current window
+void setBorder(Window *window) {
+	XSetWindowBorderWidth(display,*window,1);
+	XSetWindowBorder(display, *window, 20);
+
+}
+
+void setupEvents(Window *window) {
+    XSelectInput(display, *window, 
+			KeyPressMask
+	);
+}
+
+int main() {
+	//Open Display and Assert
+	display = XOpenDisplay(NIL);
+	assert(display);
+
+	//Get Active Screen
+	activeScreen = DefaultScreen(display);
+
+	//Create a Window and Setup Events for the Window
+	window = createWindow();
+	setupEvents(&window);
+
 
 	for (;;) {
-		event = xcb_wait_for_event(xcbConnection);
+		XNextEvent(display, &event);
+
+		displayMessage(&window);	
+		setBorder(&window);
 	}
+
+	return 0;
 }
