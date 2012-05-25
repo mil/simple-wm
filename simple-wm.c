692178f5617f1efc326001c77d4f3d39b6985655
Miles Sandlar <miles@userbound.com>
2012-05-25 14:35:08 -0400

    Cleaner Implementaiton of Event Loop Handling Events follow hXEventName naming format

--- simple-wm.c
+++ simple-wm.c
@@ -17,25 +17,16 @@ typedef struct {
 	Window				window;
 	XButtonEvent 		*button;
 	XWindowAttributes 	attributes;
+	int x;
+	int y;
 } PointerMotion;
 PointerMotion drag;
 
 
-//Display message 
-void displayMessage(Window *window, char message[]) {
-	XClearWindow(display, *window);
-	XDrawString(
-			display,
-			*window,
-			DefaultGC(display, activeScreen),
-			15, 15,
-			message, strlen(message)
-			);
-}
-
-//Creates and maps window
-int createStatusWindow() {
-
+/* ---------------------------
+ * Status Window Related
+ * --------------------------- */
+void createStatusWindow() {
 	int width = DisplayWidth(display, activeScreen);
 	int height = DisplayHeight(display, activeScreen);
 	int infoBoxHeight = 20;
@@ -49,35 +40,24 @@ int createStatusWindow() {
 			WhitePixel(display, activeScreen) //Background
 			);	
 	XMapWindow(display, statusWindow);
-
-	return statusWindow;
 }
 
-//Expands the border to be 20 pixels
-void applyBorder(Window *window) {
-	XSetWindowBorderWidth(display,*window,20);
-	XSetWindowBorder(display, *window, 20);
+void logMessage(char message[]) {
+	XClearWindow(display, statusWindow);
+	XDrawString(
+			display,
+			statusWindow,
+			DefaultGC(display, activeScreen),
+			15, 15,
+			message, strlen(message)
+			);
 }
 
-//Sets given window with cursor
-void setCursor(Window *window, int cursor) {
-	//Create the Cursor and then Define Cursor for Window
-	cursor = XCreateFontCursor(display, cursor);
-	XDefineCursor(display, *window, cursor);
-}
 
-void centerPointer(Window *window) {
-	//Get Window Attributes
-	XWindowAttributes windowAttributes;
-	XGetWindowAttributes(display, *window, &windowAttributes);
 
-	int centerX = windowAttributes.width / 2,
-			centerY = windowAttributes.height / 2;
-
-	//Warp to Center
-	XWarpPointer(display, None, *window, 0, 0, 0, 0, centerX,centerY);
-}
-
+/* ----------------------------------
+ * Window Manipulation Initializaiton 
+ * ---------------------------------- */
 //Sets up events for given window
 void setupEvents() {
 	XGrabButton(
@@ -97,138 +77,181 @@ void setupEvents() {
 			);
 }
 
+
+
+/* ---------------------------
+ * Window Manipulation Related
+ * --------------------------- */
+void applyBorder(Window *window) {
+	XSetWindowBorderWidth(display,*window,20);
+	XSetWindowBorder(display, *window, 20);
+}
+
+void setCursor(Window *window, int cursor) {
+	//Create the Cursor and then Define Cursor for Window
+	cursor = XCreateFontCursor(display, cursor);
+	XDefineCursor(display, *window, cursor);
+}
+
+void centerPointer(Window *window) {
+	//Get Window Attributes
+	XWindowAttributes windowAttributes;
+	XGetWindowAttributes(display, *window, &windowAttributes);
+
+	int centerX = windowAttributes.width / 2,
+			centerY = windowAttributes.height / 2;
+
+	//Warp to Center
+	XWarpPointer(display, None, *window, 0, 0, 0, 0, centerX,centerY);
+}
 //Raises Window, Focuses, Makes Window the Active Window
 void raiseWindow(Window *window){
 	XRaiseWindow(display, *window);
 	activeWindow = *window;
 }
 
-void mapWindow(Window *window) {
 
-	XMapWindow(display, *window);
-	applyBorder(window);
-	centerPointer(window);
-	XSelectInput(display, *window, 
-			FocusChangeMask | KeyPressMask | ButtonPressMask 
-			);
+
+/* ---------------------------
+ * Event Handlers
+ * --------------------------- */
+void hMapRequest(XEvent *event) {
+	logMessage("Map Request");
+
+	//Map the Window
+	Window mapRequestWindow = event -> xmaprequest.window;
+	XMapWindow(display, mapRequestWindow);
+
+	//Window Fns: Raise, Border, Center Pointer, Setup Events
+	raiseWindow(&mapRequestWindow);
+	applyBorder(&mapRequestWindow);
+	centerPointer(&mapRequestWindow);
+
+	/*applyBorder();
+		centerPointer(window);
+		XSelectInput(display, *window, 
+		FocusChangeMask | KeyPressMask | ButtonPressMask 
+		);
+		*/
 }
 
 //Handles Keypress, takes in modifier and keycode
-void keyPress(int modifier, int keycode) {
+void hKeyPress(XEvent *event) {
 
-	XWindowAttributes attributes;
-	int moveX = 0, moveY = 0;
+	/*
+		 keyPress(event.xkey.state, event.xkey.keycode); 
 
-	if (activeWindow != NIL && modifier == (Mod1Mask||Mod2Mask||Mod3Mask||Mod4Mask)) {
-		switch (keycode) {
-			case 113:
-				moveX = -10; //left
-				break;
-			case 114:
-				moveX = 10; //right
-				break;
-			case 111:
-				moveY = -10; //up
-				break;
-			case 116:
-				moveY = 10; //down
-				break;
-		}
 
-		XGetWindowAttributes(display, activeWindow, &attributes);
-		XMoveWindow(display, activeWindow, 
-				attributes.x + moveX, 
-				attributes.y + moveY
-				);
-	}
+		 XWindowAttributes attributes;
+		 int moveX = 0, moveY = 0;
+
+		 if (activeWindow != NIL && modifier == (Mod1Mask||Mod2Mask||Mod3Mask||Mod4Mask)) {
+		 switch (keycode) {
+		 case 113:
+		 moveX = -10; //left
+		 break;
+		 case 114:
+		 moveX = 10; //right
+		 break;
+		 case 111:
+		 moveY = -10; //up
+		 break;
+		 case 116:
+		 moveY = 10; //down
+		 break;
+		 }
+
+		 XGetWindowAttributes(display, activeWindow, &attributes);
+		 XMoveWindow(display, activeWindow, 
+		 attributes.x + moveX, 
+		 attributes.y + moveY
+		 );
+		 }
+		 */
 }
 
-void buttonPress(int button, int x, int y, Window *window) {
+void hButtonPress(XEvent *event) {
+
+	//void buttonPress(int button, int type, int x, int y, Window *window) {
+	/*
+		 buttonPress(
+		 event.xbutton.button, 
+		 event.type,
+		 event.xbutton.x_root, event.xbutton.y_root, 
+		 &event.xbutton.subwindow
+		 );		
+
 	//Left Click -- Click to Focus
 	if (button == 1) {
-		if (*window) {
-			//	displayMessage(&statusWindow, "Clicking to Focus");
-			raiseWindow(window); //Or &*, just passes the pointer
-		} else {
-			//Click first button on root window
-		}
-	}
-
-	if (*window && drag.button != None ) {
-		displayMessage(&statusWindow, "DRAGOR");
-
-		XMoveResizeWindow(
-				display, *window,
-				x + (x - drag.button -> x_root),
-				y + (y - drag.button -> y_root),
-				1,
-				1
-				);
-	}
-
-}
-
-//Stores Dragging Motion in drag struct
-void motionInWindow(Window *window, XButtonEvent *button) {
-
 	if (*window) {
-		drag.window = *window;
-		drag.button = button;
-		XGetWindowAttributes(display, *window, &drag.attributes);
+	//	logMessage(&statusWindow, "Clicking to Focus");
+	raiseWindow(window); //Or &*, just passes the pointer
+	} else {
+	//Click first button on root window
 	}
+	}
+
+	if (*window && drag.button != None  && type == ButtonRelease ) {
+	logMessage(&statusWindow, "DRAGOR");
+
+	if (button == 1) {
+	XMoveWindow(
+	display, *window,
+	(drag.attributes.x) + (drag.button -> x_root - x),
+	(drag.attributes.y) + (drag.button -> y_root - y)
+	);
+	}
+	}
+
+*/
+
 }
+
+//void motionInWindow(Window *window, XButtonEvent *button) {
+void hMotionNotify(XEvent *event) {	
+	/*
+		 motionInWindow(&event.xbutton.subwindow, &event.xbutton);
+		 logMessage(&statusWindow, "Motion Notify");
+
+		 if (*window) {
+		 drag.window = *window;
+		 drag.button = button;
+		 XGetWindowAttributes(display, *window, &drag.attributes);
+		 }
+
+*/
+}
+
+void hFocusIn(XEvent *event) {
+	/*
+		 logMessage(&statusWindow, "Focus In");
+		 raiseWindow(&event.xfocus.window);
+		 */
+}
+
+void hFocusOut(XEvent *event) {
+	// logMessage(&statusWindow, "Focus Out");
+}
+
+void hPropertyNotify(XEvent *event) {
+
+}
+
 
 void handleEvent() {
 	//Waits for the Next Event (Blocking)
 	XNextEvent(display, &event);
 
 	switch (event.type) {
-		case KeyPress: 
-			keyPress(event.xkey.state, event.xkey.keycode); 
-			break;
-
-		case ButtonPress:
-			buttonPress(
-					event.xbutton.button, 
-					event.xbutton.x_root, event.xbutton.y_root, 
-					&event.xbutton.subwindow
-					);			
-			break;
-
-			// Motion within a window
-		case MotionNotify:
-			motionInWindow(&event.xbutton.subwindow, &event.xbutton);
-			displayMessage(&statusWindow, "Motion Notify");
-			break;
-
-		case CreateNotify:
-			displayMessage(&statusWindow, "Create Notify");
-			break;
-
-		case MapRequest:
-			displayMessage(&statusWindow, "Map Request");
-			XMapRequestEvent mapRequest = event.xmaprequest;
-			mapWindow(&mapRequest.window);
-			raiseWindow(&mapRequest.window);
-			break;
-
-		case FocusIn:
-			displayMessage(&statusWindow, "Focus In");
-			raiseWindow(&event.xfocus.window);
-
-			break;
-
-		case FocusOut:
-			displayMessage(&statusWindow, "Focus Out");
-			break;
-
-		case PropertyNotify:
-			displayMessage(&statusWindow, "property ntoify");
-			break;
-
-		default:
-			//	displayMessage(&window, "Unknown Event");
-			break;
+		case KeyPress:       hKeyPress(&event);       break;
+		case ButtonPress:    hButtonPress(&event);    break;
+		case MotionNotify:   hMotionNotify(&event);   break;
+		case CreateNotify:   logMessage("Create Notify"); break;
+		case MapRequest:     hMapRequest(&event);     break; 
+		case FocusIn:        hFocusIn(&event);        break;
+		case FocusOut:       hFocusOut(&event);       break;
+		case PropertyNotify: hPropertyNotify(&event); break;
+		default: break;
 	}
 }
 
@@ -245,13 +268,10 @@ int main() {
 
 	//Create a Window and Setup Events for the Window
 	//Set Cursor for window only to XC_gumby
-	statusWindow = createStatusWindow();
+	createStatusWindow();
 	setCursor(&statusWindow, 56);
 
-	while (True) {
-		//Enter the Event Loop
-		handleEvent();
-	}
+	while (True) { handleEvent(); }
 
 	//If Event Loop Were to Break
 	XCloseDisplay(display);
